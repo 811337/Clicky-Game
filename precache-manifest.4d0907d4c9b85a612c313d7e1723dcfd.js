6a201b6dfa27965a3c2756f28d9c149e0a3e8bfb
Jesse Chen <811337@gmail.com>
2019-06-10 17:46:08 -0500

    Updates

---
+++ precache-manifest.4d0907d4c9b85a612c313d7e1723dcfd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7141efe3c9644ae1480ba4e6996f27a0",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "cd73c3547d6d9d7acb05",
+    "url": "/Clicky-Game/static/css/main.19bf5232.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "cd73c3547d6d9d7acb05",
+    "url": "/Clicky-Game/static/js/main.35f32b32.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "904222a554e3103a94244ffddffbb8a7",
+    "url": "/Clicky-Game/static/media/Applejack.904222a5.jpg"
+  },
+  {
+    "revision": "51300490ce6fd70a1a3370ab00240895",
+    "url": "/Clicky-Game/static/media/Blaze.51300490.jpg"
+  },
+  {
+    "revision": "aab47ba476e9b477c69a2bd08e21596b",
+    "url": "/Clicky-Game/static/media/Celestia.aab47ba4.jpg"
+  },
+  {
+    "revision": "01060fc9d7a66c5563f446489820f50e",
+    "url": "/Clicky-Game/static/media/Dashy.01060fc9.jpg"
+  },
+  {
+    "revision": "9208810810ee5853af13854cd176db8b",
+    "url": "/Clicky-Game/static/media/Daybreaker.92088108.jpg"
+  },
+  {
+    "revision": "eae516ae1223f36f29c61a3cbc39446a",
+    "url": "/Clicky-Game/static/media/Fluttershy.eae516ae.jpg"
+  },
+  {
+    "revision": "fffa543c7f70a3af39494f3bdd1f9d18",
+    "url": "/Clicky-Game/static/media/Luna.fffa543c.jpg"
+  },
+  {
+    "revision": "cf7f31523f6120368831a444fdf385e7",
+    "url": "/Clicky-Game/static/media/Nightmare.cf7f3152.jpg"
+  },
+  {
+    "revision": "de5cbe18a4ec104bcaeddc39e48a686c",
+    "url": "/Clicky-Game/static/media/Pinky.de5cbe18.jpg"
+  },
+  {
+    "revision": "0f1cbd15a875ab623c4d69e63f15f872",
+    "url": "/Clicky-Game/static/media/Rarity.0f1cbd15.jpg"
+  },
+  {
+    "revision": "7ded9967e8c43583fa69203955a97439",
+    "url": "/Clicky-Game/static/media/Trixie.7ded9967.jpg"
+  },
+  {
+    "revision": "c6428b50ad829cd18cae294e21a36f4b",
+    "url": "/Clicky-Game/static/media/Twilight.c6428b50.jpg"
+  }
+]);
